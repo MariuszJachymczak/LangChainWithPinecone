24765cedd9ecab8a5319ac614c871cb76447218d
mariusz.jachymczak <mariusz.jachymczak@gazeta.pl>
2023-12-21 20:15:39 +0100

    adding basic logic

--- CustomChat/src/App.tsx
+++ CustomChat/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LangchainRender from './components/langchain/LangchainMain'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1></h1>
+      <LangchainRender />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
--- CustomChat/src/components/langchain/LangchainMain.tsx
+++ CustomChat/src/components/langchain/LangchainMain.tsx
@@ -1,9 +1,40 @@
-import { OpenAI } from "langchain/llms/openai";
-import dotenv from "dotenv";
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { HumanMessage } from '@langchain/core/messages';
+import { ChatPromptTemplate } from 'langchain/prompts';
 
-dotenv.config();
-const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
-console.log (apiKey)
-const llm = new OpenAI({
-  openAIApiKey: "YOUR_KEY_HERE",
+const apiKey = import.meta.env.VITE_REACT_APP_OPENAI_API_KEY;
+
+const chatModel = new ChatOpenAI({ openAIApiKey: apiKey, temperature: 0.2 });
+
+const text =
+  'What would be a good company name for a company that makes colorful socks?';
+
+const messages = [new HumanMessage({ content: text })];
+const textInput = messages.map(message => message.content).join(' ');
+const chatModelResult = await chatModel.predict(textInput);
+
+const template = `You are a helpful assistance that translate {input_language} into {output_language}.`;
+
+const humanTemplate = '{assistanceText}';
+
+const chatPrompt = ChatPromptTemplate.fromMessages([
+  ['system', template],
+  ['human', humanTemplate],
+]);
+
+const formattedChatPrompt = await chatPrompt.format({
+  input_language: 'English',
+  output_language: 'Japanese',
+  assistanceText:
+    'Hello, I am a helpful assistance that translate English into Japanese.',
 });
+
+const LangchainRender: React.FC = () => {
+  return (
+    <div>
+      <h1>{chatModelResult}</h1>
+      <h1>{formattedChatPrompt}</h1>
+    </div>
+  );
+};
+export default LangchainRender;
